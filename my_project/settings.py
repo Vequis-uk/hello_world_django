8a1b1b85e9f0606d419e6fbfd11a3f942c2b9865
David Bonser <david@codapt.co.uk>
2024-12-19 15:43:27 +0000

    settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yd7+p_)&sop6%rz!8y-rllu*n53=ec=&(-_zw&vkq7$vzzbrf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-nielmc-django-project-lxqprmm3gz.us2.codeanyapp.com','.herokuapp.com']
 
 # Application definition
 
